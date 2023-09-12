92b2f11c47ce9755ad1554d2afe3c818c588ca01
Ritesh Thakare <128716512+rkvines@users.noreply.github.com>
2023-09-12 20:01:45 +0530

    added index.html , script.js , style.css

---
+++ script.js
@@ -0,0 +1,8 @@
+function search(){
+    document.querySelector('button').innerHTML='<i class="fa fa-circle-o-notch fa-spin"></i>';
+}
+
+function opensearch(){
+    document.getElementById('type').style.display='block';
+    document.getElementById('btn').style.display='block';
+}
